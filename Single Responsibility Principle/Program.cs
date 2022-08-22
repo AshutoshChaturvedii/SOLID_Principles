090a6b406ba8750a078d80766d4198de0db1dbcc
Ashutosh <ashutosh.chaturvedi@euromonitor.com>
2022-08-22 19:42:56 +0530

    Single responsibility Design principle

---
+++ Single Responsibility Principle/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Single_Responsibility_Principle
+{
+    //Here we have segregated all the classes to  ensure every class is having single responsibility
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public interface ILogger
+    {
+        void Debug(string info);
+    }
+    public class Logger :
+        ILogger
+    {
+
+        public Logger()
+        {
+
+        }
+        public void Debug(string info)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class MailSender
+    {
+
+        public MailSender()
+        {
+
+        }
+        public string EmailSender { get; set; }
+        public void SendEmail()
+        {
+        }
+
+
+    }
+    public class Invoice
+    {
+        public long InvAmount { get; set; }
+        private ILogger FileLogger;
+        private MailSender Emailsender;
+        public Invoice()
+        {
+            FileLogger = new Logger();
+            Emailsender = new MailSender();
+        }
+    }
+}
